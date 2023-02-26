39444d1caae84a49829448aaa198801f12716277
Miguel Ángel González Santamarta <mgons@unileon.es>
2023-02-26 20:41:47 +0100

    results loop fixed stream=True result.cpu()

--- yolov8_ros/yolov8_ros/yolov8_node.py
+++ yolov8_ros/yolov8_ros/yolov8_node.py
@@ -67,61 +67,63 @@ class Yolov8Node(Node):
 
             # convert image + predict
             cv_image = self.cv_bridge.imgmsg_to_cv2(msg)
-            results = self.yolo.predict(source=cv_image, verbose=False)[0]
-
-            if len(results) == 0:
-                return
+            results = self.yolo.predict(
+                source=cv_image, verbose=False, stream=True)
 
             # create detections msg
             detections_msg = Detection2DArray()
             detections_msg.header = msg.header
 
-            for b in results.boxes:
+            for result in results:
 
-                label = self.yolo.names[int(b.cls.cpu())]
-                score = float(b.conf.cpu())
+                result = result.cpu()
 
-                if score < self.threshold:
-                    continue
+                for b in result.boxes:
 
-                detection = Detection2D()
+                    label = self.yolo.names[int(b.cls)]
+                    score = float(b.conf)
 
-                box = b.xywh[0].cpu()
+                    if score < self.threshold:
+                        continue
 
-                # get boxes values
-                detection.bbox.center.x = float(box[0])
-                detection.bbox.center.y = float(box[1])
-                detection.bbox.size_x = float(box[2])
-                detection.bbox.size_y = float(box[3])
+                    detection = Detection2D()
 
-                # get hypothesis
-                hypothesis = ObjectHypothesisWithPose()
-                hypothesis.hypothesis.class_id = label
-                hypothesis.hypothesis.score = score
-                detection.results.append(hypothesis)
+                    box = b.xywh[0]
 
-                # draw boxes for debug
-                if label not in self._class_to_color:
-                    r = random.randint(0, 255)
-                    g = random.randint(0, 255)
-                    b = random.randint(0, 255)
-                    self._class_to_color[label] = (r, g, b)
-                color = self._class_to_color[label]
+                    # get boxes values
+                    detection.bbox.center.x = float(box[0])
+                    detection.bbox.center.y = float(box[1])
+                    detection.bbox.size_x = float(box[2])
+                    detection.bbox.size_y = float(box[3])
 
-                min_pt = (round(detection.bbox.center.x - detection.bbox.size_x / 2.0),
-                          round(detection.bbox.center.y - detection.bbox.size_y / 2.0))
-                max_pt = (round(detection.bbox.center.x + detection.bbox.size_x / 2.0),
-                          round(detection.bbox.center.y + detection.bbox.size_y / 2.0))
-                cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
+                    # get hypothesis
+                    hypothesis = ObjectHypothesisWithPose()
+                    hypothesis.hypothesis.class_id = label
+                    hypothesis.hypothesis.score = score
+                    detection.results.append(hypothesis)
 
-                label = "{} {:.3f}".format(label, score)
-                pos = (min_pt[0] + 5, min_pt[1] + 20)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(cv_image, label, pos, font,
-                            1, color, 1, cv2.LINE_AA)
+                    # draw boxes for debug
+                    if label not in self._class_to_color:
+                        r = random.randint(0, 255)
+                        g = random.randint(0, 255)
+                        b = random.randint(0, 255)
+                        self._class_to_color[label] = (r, g, b)
+                    color = self._class_to_color[label]
 
-                # append msg
-                detections_msg.detections.append(detection)
+                    min_pt = (round(detection.bbox.center.x - detection.bbox.size_x / 2.0),
+                              round(detection.bbox.center.y - detection.bbox.size_y / 2.0))
+                    max_pt = (round(detection.bbox.center.x + detection.bbox.size_x / 2.0),
+                              round(detection.bbox.center.y + detection.bbox.size_y / 2.0))
+                    cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
+
+                    label = "{} {:.3f}".format(label, score)
+                    pos = (min_pt[0] + 5, min_pt[1] + 20)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(cv_image, label, pos, font,
+                                1, color, 1, cv2.LINE_AA)
+
+                    # append msg
+                    detections_msg.detections.append(detection)
 
             # publish detections and dbg image
             self._pub.publish(detections_msg)
