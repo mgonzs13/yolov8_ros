65b82dc0f924ebe82d73be836b302ffcb4c8a827
Miguel Ángel González Santamarta <mgons@unileon.es>
2023-03-16 17:53:54 +0100

    ultralytics tracking added

--- yolov8_ros/yolov8_ros/yolov8_node.py
+++ yolov8_ros/yolov8_ros/yolov8_node.py
@@ -1,13 +1,18 @@
 
 import cv2
+import torch
 import random
 
 import rclpy
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
-
 from cv_bridge import CvBridge
+
 from ultralytics import YOLO
+from ultralytics.tracker import BOTSORT, BYTETracker
+from ultralytics.tracker.trackers.basetrack import BaseTrack
+from ultralytics.yolo.utils import IterableSimpleNamespace, yaml_load
+from ultralytics.yolo.utils.checks import check_requirements, check_yaml
 
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2D
@@ -23,7 +28,12 @@ class Yolov8Node(Node):
 
         # params
         self.declare_parameter("model", "yolov8m.pt")
-        model = self.get_parameter("model").get_parameter_value().string_value
+        model = self.get_parameter(
+            "model").get_parameter_value().string_value
+
+        self.declare_parameter("tracker", "bytetrack.yaml")
+        tracker = self.get_parameter(
+            "tracker").get_parameter_value().string_value
 
         self.declare_parameter("device", "cuda:0")
         device = self.get_parameter(
@@ -39,7 +49,9 @@ class Yolov8Node(Node):
 
         self._class_to_color = {}
         self.cv_bridge = CvBridge()
+        self.tracker = self.create_tracker(tracker)
         self.yolo = YOLO(model)
+        self.yolo.fuse()
         self.yolo.to(device)
 
         # topcis
@@ -52,6 +64,19 @@ class Yolov8Node(Node):
 
         # services
         self._srv = self.create_service(SetBool, "enable", self.enable_cb)
+
+    def create_tracker(self, tracker_yaml) -> BaseTrack:
+
+        TRACKER_MAP = {"bytetrack": BYTETracker, "botsort": BOTSORT}
+        check_requirements("lap")  # for linear_assignment
+
+        tracker = check_yaml(tracker_yaml)
+        cfg = IterableSimpleNamespace(**yaml_load(tracker))
+
+        assert cfg.tracker_type in ["bytetrack", "botsort"], \
+            f"Only support 'bytetrack' and 'botsort' for now, but got '{cfg.tracker_type}'"
+        tracker = TRACKER_MAP[cfg.tracker_type](args=cfg, frame_rate=1)
+        return tracker
 
     def enable_cb(self,
                   req: SetBool.Request,
@@ -68,62 +93,82 @@ class Yolov8Node(Node):
             # convert image + predict
             cv_image = self.cv_bridge.imgmsg_to_cv2(msg)
             results = self.yolo.predict(
-                source=cv_image, verbose=False, stream=True)
+                source=cv_image,
+                verbose=False,
+                stream=False,
+                conf=0.1,
+                mode="track"
+            )
+
+            # track
+            det = results[0].boxes.cpu().numpy()
+
+            if len(det) > 0:
+                im0s = self.yolo.predictor.batch[2]
+                im0s = im0s if isinstance(im0s, list) else [im0s]
+
+                tracks = self.tracker.update(det, im0s[0])
+                if len(tracks) > 0:
+                    results[0].update(boxes=torch.as_tensor(tracks[:, :-1]))
 
             # create detections msg
             detections_msg = Detection2DArray()
             detections_msg.header = msg.header
 
-            for result in results:
+            results = results[0].cpu()
 
-                result = result.cpu()
+            for b in results.boxes:
 
-                for b in result.boxes:
+                label = self.yolo.names[int(b.cls)]
+                score = float(b.conf)
 
-                    label = self.yolo.names[int(b.cls)]
-                    score = float(b.conf)
+                if score < self.threshold:
+                    continue
 
-                    if score < self.threshold:
-                        continue
+                detection = Detection2D()
 
-                    detection = Detection2D()
+                box = b.xywh[0]
 
-                    box = b.xywh[0]
+                # get boxes values
+                detection.bbox.center.position.x = float(box[0])
+                detection.bbox.center.position.y = float(box[1])
+                detection.bbox.size_x = float(box[2])
+                detection.bbox.size_y = float(box[3])
 
-                    # get boxes values
-                    detection.bbox.center.position.x = float(box[0])
-                    detection.bbox.center.position.y = float(box[1])
-                    detection.bbox.size_x = float(box[2])
-                    detection.bbox.size_y = float(box[3])
+                # get track id
+                track_id = -1
+                if not b.id is None:
+                    track_id = int(b.id)
+                detection.id = str(track_id)
 
-                    # get hypothesis
-                    hypothesis = ObjectHypothesisWithPose()
-                    hypothesis.hypothesis.class_id = label
-                    hypothesis.hypothesis.score = score
-                    detection.results.append(hypothesis)
+                # get hypothesis
+                hypothesis = ObjectHypothesisWithPose()
+                hypothesis.hypothesis.class_id = label
+                hypothesis.hypothesis.score = score
+                detection.results.append(hypothesis)
 
-                    # draw boxes for debug
-                    if label not in self._class_to_color:
-                        r = random.randint(0, 255)
-                        g = random.randint(0, 255)
-                        b = random.randint(0, 255)
-                        self._class_to_color[label] = (r, g, b)
-                    color = self._class_to_color[label]
+                # draw boxes for debug
+                if label not in self._class_to_color:
+                    r = random.randint(0, 255)
+                    g = random.randint(0, 255)
+                    b = random.randint(0, 255)
+                    self._class_to_color[label] = (r, g, b)
+                color = self._class_to_color[label]
 
-                    min_pt = (round(detection.bbox.center.position.x - detection.bbox.size_x / 2.0),
-                              round(detection.bbox.center.position.y - detection.bbox.size_y / 2.0))
-                    max_pt = (round(detection.bbox.center.position.x + detection.bbox.size_x / 2.0),
-                              round(detection.bbox.center.position.y + detection.bbox.size_y / 2.0))
-                    cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
+                min_pt = (round(detection.bbox.center.position.x - detection.bbox.size_x / 2.0),
+                          round(detection.bbox.center.position.y - detection.bbox.size_y / 2.0))
+                max_pt = (round(detection.bbox.center.position.x + detection.bbox.size_x / 2.0),
+                          round(detection.bbox.center.position.y + detection.bbox.size_y / 2.0))
+                cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
 
-                    label = "{} {:.3f}".format(label, score)
-                    pos = (min_pt[0] + 5, min_pt[1] + 20)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(cv_image, label, pos, font,
-                                1, color, 1, cv2.LINE_AA)
+                label = "{} ({}) ({:.3f})".format(label, str(track_id), score)
+                pos = (min_pt[0] + 5, min_pt[1] + 25)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(cv_image, label, pos, font,
+                            1, color, 1, cv2.LINE_AA)
 
-                    # append msg
-                    detections_msg.detections.append(detection)
+                # append msg
+                detections_msg.detections.append(detection)
 
             # publish detections and dbg image
             self._pub.publish(detections_msg)
